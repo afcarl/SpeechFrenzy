1436359ba3ec4f322eb0d88670f82064ca75e691
CodeBreaker <>
2016-02-21 01:03:26 +0000

    fixed slicin

--- fileSlice.py
+++ fileSlice.py
@@ -2,8 +2,6 @@
 import sys
 sys.path.append("./lib")
 
-
-import xml.etree.ElementTree as ET
 from pydub import AudioSegment
 
 INPUT_DIR = "./input/"
@@ -11,34 +9,18 @@ OUTPUT_DIR = "./output/"
 START_TAG = "start"
 DUR_TAG = "dur"
 
-
-
-def trim( filename ):
+def trim(filename, timeOne, timeTwo, captionNo):
    success = 0;
-   root = ET.parse(INPUT_DIR+filename+'.xml').getroot()
    #TODO deal with >>, different people speaking etc
-   ogg_version = AudioSegment.from_ogg(INPUT_DIR+filename+".ogg")
+   # ogg_version = AudioSegment.from_ogg(INPUT_DIR+filename+".ogg")
+   webm_version = AudioSegment.from_ogg(INPUT_DIR+filename+".webm")
+   split = song[timeOne:timeTwo]
 
-   halfway_point = len(ogg_version) / 2
-   second_half = ogg_version[halfway_point:]
-
-   second_half_3_times.export(OUTPUT_DIR+"file.ogg", format="ogg")
-   curTime = 0
-
-   for child in root:
-   	if curTime != child.get(START_TAG):
-		curTime = child.get(START_TAG)
-   	
-   	endTime = curTime + child.get(DUR_TAG)
+   split.export(OUTPUT_DIR+filename+"-"+captionNo+".wav", format="wav")
+   return success;
 
 
-
-	print child.tag, child.attrib
-	print child.text
-	return success;
-
-
-trim('Eo-Drako')
+# trim('Eo-Drako')
 
 #http://stackoverflow.com/questions/193077/standalone-python-applications-in-linux
 #http://undefined.org/python/py2app.html
